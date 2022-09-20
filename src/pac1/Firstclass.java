a777256ac8437c40fd6b80ab4bebd0efc1702d90
waghvaibhav <ivaibhavwagh@gmail.com>
2022-09-20 10:53:49 +0530

    First commit

---
+++ src/pac1/Firstclass.java
@@ -0,0 +1,26 @@
+package pac1;
+
+public class Firstclass {
+	
+	
+	public static void m1(){
+		
+		System.out.println("vaibhav");
+	}
+	
+	public void m2() {
+		
+		System.out.println("wagh");
+	}
+	
+	public static void main (String [] args ) {
+		
+		
+		m1();
+		
+		
+		Firstclass c = new Firstclass();
+		c.m2();
+	}
+
+}
